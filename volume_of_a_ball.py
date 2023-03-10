ffe00b4bbe429959b581626e853a25ea8820ac41
jubranakram <jubranakram@gmail.com>
2023-03-09 19:53:15 -0500

    added figure code

--- volume_of_a_ball.py
+++ volume_of_a_ball.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
 
 '''
 Calculate the volume of a ball defined by x**2 + y**2 + z**2 <= 1
@@ -14,8 +15,21 @@ if __name__ == "__main__":
     r_squared = (r**2).sum(axis=0)
     # check if x**2 + y**2 < 1 is met
     r_idx = r_squared < 1
-    # sum the height
-    z = np.sqrt(1 - r_squared[r_idx]).sum()
+    # sum the height z = sqrt(1 - x**2 - y**2)
+    z = np.sqrt(1 - r_squared[r_idx])
+    z_sum = z.sum()
 
-    print(f'The expectation value: {z/r_idx.sum()*2*np.pi}')
+    print(f'The expectation value: {z_sum/r_idx.sum()*2*np.pi}')
     print(f'Volume of a sphere: {np.pi*4/3}')
+
+    ax = plt.axes(projection ="3d")
+
+    ax.scatter3D(r[0, r_idx], r[1, r_idx], z, color = "green", alpha=0.1)
+    ax.set_xlabel('$x$')
+    ax.set_ylabel('$y$')
+    ax.set_zlabel('$z$')
+    ax.grid()
+    
+    
+    # show plot
+    plt.show()
